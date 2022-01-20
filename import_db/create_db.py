a8cd4a62f530611151fd3309a2ed8cb9c797b260
Kurk3 <kurekadam314@gmail.com>
2022-01-20 17:30:43 +0100

    commiting new files

--- import_db/create_db.py
+++ import_db/create_db.py
@@ -4,9 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DB_NAME = "AppData"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 def create_engine(app):
-   app.config['SECRET_KEY'] = 'abcdefghfijklmnoprsqxyz'  # ked stranka ide do produkcie (heslo)
+   app.config['SECRET_KEY'] = 'abcdefghfijklmnoprsqxyz'# ked stranka ide do produkcie (heslo)
+   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql:///{DB_NAME}'  # ulozeny nazov databazy
    db.init_app(app)
 
