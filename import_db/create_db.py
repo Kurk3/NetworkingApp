54b2c58ec1600ebb6a1904643c6564241726930a
Kurk3 <kurekadam314@gmail.com>
2022-04-01 20:49:11 +0200

    commit na github

--- import_db/create_db.py
+++ import_db/create_db.py
@@ -7,5 +7,5 @@ DB_NAME = "AppData"
 def create_engine(app):
     app.config['SECRET_KEY'] = 'abcdefghfijklmnoprsqxyz'  # ked stranka ide do produkcie (heslo)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['DATABASE_URL'] = f'postgresql:///{DB_NAME}'  # sem   konekt
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql:///{DB_NAME}'
     db.init_app(app)
